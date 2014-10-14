8009f0ba886ee0baae8e7a919c8cfab8893cd1c9
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-14 07:49:56 +0300

    Initial Commit

---
+++ C_Programming_UMass/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_Programming_UMass
+//
+//  Created by Yehuda Miller on 14/10/14.
+//  Copyright (c) 2014 Yehuda Miller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
