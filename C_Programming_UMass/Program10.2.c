f380d5e477f637abd0a1f4b66a9e83a184934358
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-14 21:35:39 +0300

    commit

---
+++ C_Programming_UMass/Program10.2.c
@@ -0,0 +1,27 @@
+// Function to calculate the number of characters in a string
+
+#include <stdio.h>
+
+int string_length (const char string[])
+{
+    int count = 0;
+    
+    while (string[count] != '\0') {
+        ++count;
+    }
+    
+    return count;
+}
+
+int main (void)
+{
+    int string_length (const char string[]);
+    const char word1[] = {'A', 's', 't', 'e', 'r', '\0'};
+    const char word2[] = {'a', 't', '\0'};
+    const char word3
+    
+    
+    
+    
+    return 0;
+}
