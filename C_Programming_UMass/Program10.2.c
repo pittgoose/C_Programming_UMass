360e931ac7b10c37a7344da1e94081651c5a4fa5
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-15 09:38:58 +0300

    morning commit

--- C_Programming_UMass/Program10.2.c
+++ C_Programming_UMass/Program10.2.c
@@ -18,8 +18,9 @@ int main (void)
     int string_length (const char string[]);
     const char word1[] = {'A', 's', 't', 'e', 'r', '\0'};
     const char word2[] = {'a', 't', '\0'};
-    const char word3
+    const char word3[] = {'a', 'w', 'e', '\0'};
     
+    printf("%i\t%i\t%i\n", string_length(word1), string_length(word2), string_length(word3));
     
     
     
---
+++ C_Programming_UMass/Program10.3.c
@@ -0,0 +1,18 @@
+// combining program 10.1 and 10.2
+
+#include <stdio.h>
+
+int main (void)
+{
+    void concat (char result[], const char str1[], const char str2[]);
+    const char s1[] = {"Test "};
+    const char s2[] = {"works"};
+    char s3[20];
+    
+    printf("%s\n", s3);
+    
+    
+    
+    
+    return 0;
+}
