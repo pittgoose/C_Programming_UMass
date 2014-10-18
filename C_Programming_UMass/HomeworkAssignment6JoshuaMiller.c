ac49256c034acecca321d0879e57162c5b262511
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-18 20:52:59 +0300

    assignment 6 started

---
+++ C_Programming_UMass/HomeworkAssignment6JoshuaMiller.c
@@ -0,0 +1,21 @@
+// *******************************************************************
+//
+// HOMEWORK: Assignment 6 - Strings
+//
+// Name: Joshua Miller
+//
+// Class: C Programming
+//
+// Date: 18 October, 2014
+//
+// Description: This program prompts the user for the number of hours
+//              worked for each employee. It then calculates gross
+//              pay including overtime and displays the results in
+//              table. Functions and structures are used as well as
+//              strings from the C String Library.
+//
+// *******************************************************************
+
+#include <stdio.h>
+#include <string.h>
+
