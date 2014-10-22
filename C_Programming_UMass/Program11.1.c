b56c4fa5b47b1cb0a7579dd1caf787b1c25e4dc9
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-22 17:46:44 +0300

    chapter 11 work

---
+++ C_Programming_UMass/Program11.1.c
@@ -0,0 +1,18 @@
+// Program to illustrate pointers
+
+#include <stdio.h>
+
+int main (void)
+{
+    int count = 10;
+    int x;
+    int *int_pointer;
+    
+    int_pointer = &count;
+    x = *int_pointer;
+    
+    printf("count = %i, x = %i\n", count, x);
+    
+    return 0;
+    
+}
