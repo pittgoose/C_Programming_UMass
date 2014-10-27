3201f16545e0c2513b9004644d9df0474d28eb62
Yehuda Miller <yehudafmiller@gmail.com>
2014-10-27 17:53:45 -0400

    Oh say can you see?

---
+++ C_Programming_UMass/Program11.14.c
@@ -0,0 +1,26 @@
+// Pointer version of copystring v2
+
+#include <stdio.h>
+
+void copyString (char *to, char *from)
+{
+    while (*from) {
+        *to++ = *from++;
+    }
+    *to = '\0';
+}
+
+int main (void)
+{
+    void copyString (char *to, char *from);
+    char string1[] = "A string to be copied.";
+    char string2[50];
+    
+    copyString(string2, string1);
+    printf("%s\n", string2);
+    
+    copyString(string2, "So is this.");
+    printf("%s\n", string2);
+    
+    return 0;
+}
