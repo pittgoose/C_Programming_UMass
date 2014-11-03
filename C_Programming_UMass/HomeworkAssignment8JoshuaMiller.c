4e58cf078de9dd5f6ca2bfd4a62f5c472afd0238
Yehuda Miller <yehudafmiller@gmail.com>
2014-11-03 22:11:53 +0200

    homework 8 final revision

--- C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
+++ C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
@@ -1,13 +1,31 @@
+// *******************************************************************
+//
+// HOMEWORK: Assignment 9 - Linked Lists
+//
+// Name: Joshua Miller
+//
+// Class: C Programming
+//
+// Date: 3 November, 2014
+//
+// Description: This program prompts the user for information about
+//              several employees. It then calculates gross
+//              pay including overtime and displays the results in a
+//              table. The data is stored in a linked list, allocating
+//              memory dynamically for each new employee node.
+//
+// *******************************************************************
+
 #include <stdio.h>
-#include <stdlib.h>   /* for malloc */
+#include <stdlib.h>   // for malloc
 #include <ctype.h>
 
 // CONSTANTS
 #define STD_HOURS 40.0f // Standard number of work hours per week
 #define OVERTIME_MULTIPLIER 1.5f // Overtime wage calculator
 #define MAX_HOURS 60 // Does not allow an employee to work more than 60 hours in a week
-#define MAX_FIRST_NAME 10 // Maximum first namename constraint.
-#define MAX_LAST_NAME 20 // Maximum first namename constraint.
+#define MAX_FIRST_NAME 10 // Maximum first name constraint.
+#define MAX_LAST_NAME 20 // Maximum first name constraint.
 
 struct employee
 {
@@ -35,7 +53,7 @@ struct employee
 // *******************************************************************
 void calculate_overtime (struct employee *emp1)
 {
-    struct employee *tmp;   /* tmp pointer value to current node */
+    struct employee *tmp; // tmp pointer value to current node
     
     for (tmp = emp1; tmp ; tmp = tmp->next) {
         // Calculate the overtime hours if an employee worked over the standard amount
@@ -62,7 +80,7 @@ void calculate_overtime (struct employee *emp1)
 // *******************************************************************
 void calculate_gross_pay (struct employee *emp1)
 {
-    struct employee *tmp;   /* tmp pointer value to current node */
+    struct employee *tmp; // tmp pointer value to current node
     
     for (tmp = emp1; tmp ; tmp = tmp->next) {
         // Calculate the gross pay if overtime was worked
@@ -92,15 +110,14 @@ void calculate_gross_pay (struct employee *emp1)
 void print_list(struct employee *emp1)
 {
     struct employee *tmp; // tmp pointer value to current node
-    int i = 0; //counts the nodes printed
     
     // Print out header information for data to be displayed
     printf ("\n----------------------------------------------------------------------------------\n");
     printf ("Name\t\t\t\t\tClock#\tWage\tHours\tOT\tGross\n");
     printf ("----------------------------------------------------------------------------------\n");
     
-    /* Start a beginning of list and print out each value               */
-    /* loop until tmp points to null (remember null is 0 or false)      */
+    // Start a beginning of list and print out each value
+    // loop until tmp points to null (remember null is 0 or false)
     for(tmp = emp1; tmp ; tmp = tmp->next)
     {
         printf ("%-10s %-20s\t\t%06li\t%.2f\t%.1f\t%.1f\t%4.2f\n", tmp->first_name, tmp->last_name ,tmp->id_number, tmp->wage, tmp->hours, tmp->overtime, tmp->gross);
@@ -109,39 +126,37 @@ void print_list(struct employee *emp1)
     
 }
 
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/* FUNCTION:  main                                                            */
-/*                                                                            */
-/* DESCRIPTION:  This function will prompt the user for an employee           */
-/*               id and wage until the user indicates they are finished.      */
-/*               At that point, a list of id and wages will be                */
-/*               generated.                                                   */
-/*                                                                            */
-/* PARAMETERS:   None                                                         */
-/*                                                                            */
-/* OUTPUTS:      None                                                         */
-/*                                                                            */
-/* CALLS:        print_list                                                   */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+// *******************************************************************
+// FUNCTION:  main
+//
+// DESCRIPTION:  This function will prompt the user for an employee
+//               id and wage until the user indicates they are finished.
+//               At that point, a list of id and wages will be
+//               generated.
+//
+// PARAMETERS:   None
+//
+// OUTPUTS:      None
+//
+// CALLS:        print_list
+// *******************************************************************
 int main ()
 {
     
-    char answer[80];       /* to see if the user wants to add more employees */
-    int more_data = 1;    /* flag to check if another employee is to be processed */
-    char value;             /* gets the first character of answer */
+    char answer[80];       // to see if the user wants to add more employees
+    int more_data = 1;     // flag to check if another employee is to be processed
+    char value;            // gets the first character of answer
     
-    struct employee *current_ptr;   /* pointer to current node */
-    struct employee *head_ptr;       /* always points to first node */
+    struct employee *current_ptr;   // pointer to current node
+    struct employee *head_ptr;      // always points to first node
     
-    /* Set up storage for first node */
+    // Set up storage for first node
     head_ptr = (struct employee *) malloc (sizeof(struct employee));
     current_ptr = head_ptr;
     
     while (more_data)
     {
-        /* Read in Employee ID and Hourly Wage */
+        // Read in Employee ID and Hourly Wage
         printf("\nEnter the employee name (first and last only): ");
         scanf("%s %s", current_ptr->first_name, current_ptr->last_name);
         
@@ -164,7 +179,7 @@ int main ()
         printf("Would you like to add another employee? (y/n): ");
         scanf("%s", answer);
         
-        /* Ask user if they want to add another employee */
+        // Ask user if they want to add another employee
         if ((value = toupper(answer[0])) != 'Y')
         {
             current_ptr->next = (struct employee *) NULL;
@@ -172,18 +187,18 @@ int main ()
         }
         else
         {
-            /* set the next pointer of the current node to point to the new node */
+            // set the next pointer of the current node to point to the new node
             current_ptr->next = (struct employee *) malloc (sizeof(struct employee));
-            /* move the current node pointer to the new node */
+            // move the current node pointer to the new node
             current_ptr = current_ptr->next;
         }
-    } /* while */
+    } // end while
     
-    /* perform the necessary calculations on the employees */
+    // perform the necessary calculations on the employees
     calculate_overtime(head_ptr);
     calculate_gross_pay(head_ptr);
     
-    /* print out listing of all employee id's and wages that were entered */
+    // print out listing of all employee id's and wages that were entered
     print_list(head_ptr);
     
     printf("\n\nEnd of program\n");
