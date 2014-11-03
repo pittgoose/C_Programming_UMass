baf6573d60520595c3387c70beeb89e0a9ec8c5b
Yehuda Miller <yehudafmiller@gmail.com>
2014-11-03 21:39:01 +0200

    Midway through the final homework.

--- C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
+++ C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
@@ -1,25 +1,16 @@
-/*
-Homework 8 Template
-
-Try this example and use it as a guide to your last homework (number 8).  You just really need to expand upon it.
-
-Add the missing members to the struct employee type, and add functions as needed (such as ones to calculate overtime and gross pay).
-
-I added comments in green below that start with TODO to indicate what needs to change.
-
-Relax, its OK if you use this code as is and then expand from it ... you have my permission to use all of it.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>   /* for malloc */
 #include <ctype.h>
 
 struct employee
 {
-    int id_number;
+    char first_name [10];
+    char last_name  [10];
+    long int id_number;
     float wage;
-    
-    /* TODO - Add other members */
+    float hours;
+    float overtime;
+    float gross;
     
     struct employee *next;
 };
@@ -44,15 +35,18 @@ void print_list(struct employee *emp1)
     struct employee *tmp;   /* tmp pointer value to current node */
     int i = 0;              /* counts the nodes printed          */
     
+    // Print out header information for data to be displayed
+    printf ("\n--------------------------------------------------------------\n");
+    printf ("Name\t\t\tClock#\tWage\tHours\tOT\tGross\n");
+    printf ("--------------------------------------------------------------\n");
+    
     /* Start a beginning of list and print out each value               */
     /* loop until tmp points to null (remember null is 0 or false)      */
     for(tmp = emp1; tmp ; tmp = tmp->next)
     {
         i++;
-        
-        /* TODO - print other members as well */
-        printf("\nEmployee ID: %6d, Wage: %8.2f\n",tmp->id_number,
-               tmp->wage);
+        printf ("%s %s\t\t%06li\t%.2f\t%.1f\t%.1f\t%4.2f\n", tmp->first_name, tmp->last_name ,tmp->id_number, tmp->wage, tmp->hours, tmp->overtime, tmp->gross);
+
     }
     
     printf("\n\nTotal Number of Employees = %d\n", i);
@@ -78,12 +72,12 @@ void print_list(struct employee *emp1)
 int main ()
 {
     
-    char   answer[80];       /* to see if the user wants to add more employees */
-    int    more_data = 1;    /* flag to check if another employee is to be processed */
-    char   value;             /* gets the first character of answer */
+    char answer[80];       /* to see if the user wants to add more employees */
+    int more_data = 1;    /* flag to check if another employee is to be processed */
+    char value;             /* gets the first character of answer */
     
-    struct employee *current_ptr,   /* pointer to current node */
-    *head_ptr;       /* always points to first node */
+    struct employee *current_ptr;   /* pointer to current node */
+    struct employee *head_ptr;       /* always points to first node */
     
     /* Set up storage for first node */
     head_ptr = (struct employee *) malloc (sizeof(struct employee));
@@ -91,16 +85,18 @@ int main ()
     
     while (more_data)
     {
-        
-        
-        /* TODO - Prompt for Employee Name and Hours as well here */
-        
         /* Read in Employee ID and Hourly Wage */
         printf("\nEnter employee ID: ");
-        scanf("%i", & current_ptr -> id_number);
+        scanf("%li", & current_ptr -> id_number);
         
         printf("\nEnter employee hourly wage: ");
         scanf("%f", & current_ptr -> wage);
+        
+        printf("\nEnter the employee name (first and last only): ");
+        scanf("%s %s", current_ptr->first_name, current_ptr->last_name);
+        
+        printf("\nEnter the number of hours worked by this employee: ");
+        scanf("%f", & current_ptr->hours);
         
         printf("Would you like to add another employee? (y/n): ");
         scanf("%s", answer);
@@ -119,6 +115,12 @@ int main ()
             current_ptr = current_ptr->next;
         }
     } /* while */
+    
+    /* perform the necessary calculations on the employees */
+    get_hours(employees, NUM_EMPLOYEES);
+    calculate_overtime(employees, NUM_EMPLOYEES);
+    calculate_gross_pay(employees, NUM_EMPLOYEES);
+    print_employee_wages(employees, NUM_EMPLOYEES);
     
     /* print out listing of all employee id's and wages that were entered */
     print_list(head_ptr);
