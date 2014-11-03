b68a17b1ffa2b4a14943e5b9551cfe0f12d526da
Yehuda Miller <yehudafmiller@gmail.com>
2014-11-03 22:03:46 +0200

    even more stable

--- C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
+++ C_Programming_UMass/HomeworkAssignment8JoshuaMiller.c
@@ -5,14 +5,14 @@
 // CONSTANTS
 #define STD_HOURS 40.0f // Standard number of work hours per week
 #define OVERTIME_MULTIPLIER 1.5f // Overtime wage calculator
-#define NUM_EMPLOYEES 5 // The program will ask for 5 entries, store 5 values in each array etc.
 #define MAX_HOURS 60 // Does not allow an employee to work more than 60 hours in a week
-#define MAX_NAME_LEN 20 // Maximum name constraint. Can now be changed here at will
+#define MAX_FIRST_NAME 10 // Maximum first namename constraint.
+#define MAX_LAST_NAME 20 // Maximum first namename constraint.
 
 struct employee
 {
-    char first_name [10];
-    char last_name  [10];
+    char first_name [MAX_FIRST_NAME];
+    char last_name  [MAX_LAST_NAME];
     long int id_number;
     float wage;
     float hours;
@@ -76,41 +76,36 @@ void calculate_gross_pay (struct employee *emp1)
     }
 }
 
-/*-----------------------------------------------------------------------------*/
-/*                                                                             */
-/* FUNCTION:  print_list                                                       */
-/*                                                                             */
-/* DESCRIPTION:  This function will print the contents of a linked             */
-/*               list.  It will traverse the list from beginning to the        */
-/*               end, printing the contents at each node.                      */
-/*                                                                             */
-/* PARAMETERS:   emp1 - pointer to a linked list                               */
-/*                                                                             */
-/* OUTPUTS:      None                                                          */
-/*                                                                             */
-/* CALLS:        None                                                          */
-/*                                                                             */
-/*-----------------------------------------------------------------------------*/
+// *******************************************************************
+// FUNCTION:  print_list
+//
+// DESCRIPTION:  This function will print the contents of a linked
+//               list.  It will traverse the list from beginning to the
+//               end, printing the contents at each node.
+//
+// PARAMETERS:   emp1 - pointer to a linked list
+//
+// OUTPUTS:      None
+//
+// CALLS:        None
+// *******************************************************************
 void print_list(struct employee *emp1)
 {
-    struct employee *tmp;   /* tmp pointer value to current node */
-    int i = 0;              /* counts the nodes printed          */
+    struct employee *tmp; // tmp pointer value to current node
+    int i = 0; //counts the nodes printed
     
     // Print out header information for data to be displayed
-    printf ("\n--------------------------------------------------------------\n");
-    printf ("Name\t\t\tClock#\tWage\tHours\tOT\tGross\n");
-    printf ("--------------------------------------------------------------\n");
+    printf ("\n----------------------------------------------------------------------------------\n");
+    printf ("Name\t\t\t\t\tClock#\tWage\tHours\tOT\tGross\n");
+    printf ("----------------------------------------------------------------------------------\n");
     
     /* Start a beginning of list and print out each value               */
     /* loop until tmp points to null (remember null is 0 or false)      */
     for(tmp = emp1; tmp ; tmp = tmp->next)
     {
-        i++;
-        printf ("%s %s\t\t%06li\t%.2f\t%.1f\t%.1f\t%4.2f\n", tmp->first_name, tmp->last_name ,tmp->id_number, tmp->wage, tmp->hours, tmp->overtime, tmp->gross);
+        printf ("%-10s %-20s\t\t%06li\t%.2f\t%.1f\t%.1f\t%4.2f\n", tmp->first_name, tmp->last_name ,tmp->id_number, tmp->wage, tmp->hours, tmp->overtime, tmp->gross);
 
     }
-    
-    printf("\n\nTotal Number of Employees = %d\n", i);
     
 }
 
@@ -147,17 +142,24 @@ int main ()
     while (more_data)
     {
         /* Read in Employee ID and Hourly Wage */
+        printf("\nEnter the employee name (first and last only): ");
+        scanf("%s %s", current_ptr->first_name, current_ptr->last_name);
+        
         printf("\nEnter employee ID: ");
         scanf("%li", & current_ptr -> id_number);
         
         printf("\nEnter employee hourly wage: ");
         scanf("%f", & current_ptr -> wage);
         
-        printf("\nEnter the employee name (first and last only): ");
-        scanf("%s %s", current_ptr->first_name, current_ptr->last_name);
         
         printf("\nEnter the number of hours worked by this employee: ");
         scanf("%f", & current_ptr->hours);
+        
+        // Make sure the hours entered is in a proper range
+        while (current_ptr->hours < 0 || current_ptr->hours > MAX_HOURS) {
+            printf("*That is not a valid number of hours.* \nPlease re-enter the number of hours worked for %s %s: \n", current_ptr->first_name, current_ptr->last_name);
+            scanf("%f", & current_ptr->hours);
+        }
         
         printf("Would you like to add another employee? (y/n): ");
         scanf("%s", answer);
