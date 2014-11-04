4c92b315cee071432bcf5b40b808dc3b2dbd7a30
Yehuda Miller <yehudafmiller@gmail.com>
2014-11-04 21:14:39 +0200

    will finish this tomorrow. need to watch walking dead now

--- C_Programming_UMass/Scratch.c
+++ C_Programming_UMass/Scratch.c
@@ -9,9 +9,46 @@
 // *******************************************************************
 
 #include <stdbool.h>
+#include <stdio.h>
 bool valid_date (int month, int day, int year)
 {
+    int number_days (int month, int year);
+    if (month < 1 || month > 12) {
+        return false;
+    }
+    if (day < 1 || day > number_days(month, year)) {
+        return false;
+    }
+    if (year < 0) {
+        return false;
+    }
+    return true;
+}
+
+int number_days (int month, int year){
+    int days;
+    bool is_leap_year (int year);
+    const int days_per_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     
+    if (is_leap_year(year) && month == 2) {
+        days = 29;
+    }
+    else {
+        days = days_per_month[month - 1];
+    }
+    return days;
+}
+
+// Function to determine if it's a leap year
+bool is_leap_year (int year) {
+    bool leap_year_flag;
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        leap_year_flag = true;
+    }
+    else {
+        leap_year_flag = false;
+    }
+    return leap_year_flag;
 }
 
 
@@ -20,13 +57,10 @@ bool valid_date (int month, int day, int year)
 
 
 
-
-
-
-
-
-
-
+int main (void)
+{
+    printf("%i\n", valid_date(2, 29, 2004));
+}
 
 
 
